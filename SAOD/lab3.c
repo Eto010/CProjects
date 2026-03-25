17813d8562234637dc6b1ad279c9e72a243f1ffe
Eto010 <kirchenkovlad123@gmail.com>
2026-03-25 18:27:25 +0700

    lab3 SAOD work

--- SAOD/lab3.c
+++ SAOD/lab3.c
@@ -2,6 +2,40 @@
 #define LEN(x) (sizeof(x)/sizeof((x)[0]))
 #include "func.h"
 
+int bubbleSort(int arr[], int n)
+{
+    int sum = ctrlSum(arr, n);
+    int lines = lineCount(arr, n);
+    int c = 0;
+    int m = 0;
+    int arrIn[n];
+    for(int i = 0; i < n; i++)
+    {
+        arrIn[i] = arr[i];
+    }
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = n - 1; j > i; j--)
+        {
+            c = c + 1;
+            if (arrIn[j] < arrIn[j - 1])
+            {
+                m = m + 1;
+                int temp = arrIn[j];
+                arrIn[j] = arrIn[j - 1];
+                m = m + 1;
+                arrIn[j - 1] = temp;
+                m = m + 1;
+            }
+        }
+    }
+    int x = c + m;
+    if ((ctrlSum(arrIn, n) == sum) && (lineCount(arrIn, n) == 1))
+    { 
+        return x;
+    }
+}
+
 int shakerSort(int arr[], int n)
 {
    int arrIn[n];
@@ -25,7 +59,9 @@ int shakerSort(int arr[], int n)
             {
                 m = m + 1;
                 int temp = arrIn[i];
+                m = m + 1;
                 arrIn[i] = arrIn[i - 1];
+                m = m + 1;
                 arrIn[i - 1] = temp;
                 k = i;
             }  
@@ -38,7 +74,9 @@ int shakerSort(int arr[], int n)
             {
                 m = m + 1;
                 int temp = arrIn[j];
+                m = m + 1;
                 arrIn[j] = arrIn[j + 1];
+                m = m + 1;
                 arrIn[j + 1] = temp;
                 k = j;
             }   
@@ -48,10 +86,7 @@ int shakerSort(int arr[], int n)
     if (sum == ctrlSum(arrIn, n) && lineCount(arrIn, n) == 1)
     {
         int x = c + m;
-        printf("%d + %d = %d\n", c, m, x);
-    }
-    else{
-        printf("ERROR\n");
+        return x;
     }
 }
 
@@ -72,34 +107,11 @@ int main()
     int *arrHigh3 = masGenHigh(300);
     int *arrHigh4 = masGenHigh(400);
     int *arrHigh5 = masGenHigh(500);
-    printf("Shaker sort : \n For 100 elements: \n High: \n" );
-    shakerSort(arrHigh1, 100);
-    printf("Rand:\n");
-    shakerSort(arrRand1, 100);
-    printf("Low: \n");
-    shakerSort(arrLow1, 100);
-    printf("For 200 elements: \n High: \n" );
-    shakerSort(arrHigh2, 200);
-    printf("Rand:\n");
-    shakerSort(arrRand2, 200);
-    printf("Low: \n");
-    shakerSort(arrLow2, 200);
-    printf("For 300 elements: \n High: \n" );
-    shakerSort(arrHigh3, 300);
-    printf("Rand:\n");
-    shakerSort(arrRand3, 300);
-    printf("Low: \n");
-    shakerSort(arrLow3, 300);
-    printf("For 400 elements: \nHigh: \n" );
-    shakerSort(arrHigh4, 400);
-    printf("Rand:\n");
-    shakerSort(arrRand4, 400);
-    printf("Low: \n");
-    shakerSort(arrLow4, 400);
-    printf("\n For 500 elements: \nHigh: \n" );
-    shakerSort(arrHigh5, 500);
-    printf("Rand:\n");
-    shakerSort(arrRand5, 500);
-    printf("Low: \n");
-    shakerSort(arrLow5, 500);
+    printf(" N | Мфакт+Сфакт пузырьковой  | Мф+Сф шейкерной \n");
+    printf("   | Убыв.  | Случ.  | Возр.  | Убыв.  | Случ.  | Возр.     \n");
+    printf("100| %d  | %d  | %d   | %d  | %d  | %d\n", bubbleSort(arrLow1, 100), bubbleSort(arrRand1, 100), bubbleSort(arrHigh1, 100), shakerSort(arrLow1, 100), shakerSort(arrRand1, 100), shakerSort(arrHigh1, 100));
+    printf("200| %d  | %d  | %d  | %d  | %d  | %d\n", bubbleSort(arrLow2, 200), bubbleSort(arrRand2, 200), bubbleSort(arrHigh2, 200), shakerSort(arrLow2, 200), shakerSort(arrRand2, 200), shakerSort(arrHigh2, 200));
+    printf("300| %d | %d | %d  | %d | %d  | %d\n", bubbleSort(arrLow3, 300), bubbleSort(arrRand3, 300), bubbleSort(arrHigh3, 300), shakerSort(arrLow3, 300), shakerSort(arrRand3, 300), shakerSort(arrHigh3, 300));
+    printf("400| %d | %d | %d  | %d | %d | %d\n", bubbleSort(arrLow4, 400), bubbleSort(arrRand4, 400), bubbleSort(arrHigh4, 400), shakerSort(arrLow4, 400), shakerSort(arrRand4, 400), shakerSort(arrHigh4, 400));
+    printf("500| %d | %d | %d | %d | %d | %d\n", bubbleSort(arrLow5, 500), bubbleSort(arrRand5, 500), bubbleSort(arrHigh5, 500), shakerSort(arrLow5, 500), shakerSort(arrRand5, 500), shakerSort(arrHigh5, 500));
 }
