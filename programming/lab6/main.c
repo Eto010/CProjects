af09c8ddfd64ec0e8263250166358b073681a339
Eto010 <kirchenkovlad123@gmail.com>
2026-06-08 02:28:39 +0700

    programmingt2 lab6 ex1

---
+++ programming/lab6/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void * thread_func(void *arg)
+{
+    for(int i = 1; i < 6; i++)
+    {
+        printf("child thread: %d\n", i);
+    }
+    printf("the thread has ended\n");
+}
+
+int main()
+{
+    pthread_t thread;
+    if(pthread_create(&thread, NULL, thread_func, NULL))
+    {
+        printf("thread creation error\n");
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 1; i < 6; i++)
+    {
+        printf("parent thread: %d\n", i);
+    }
+    printf("the thread has ended\n");
+}
