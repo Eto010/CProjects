ba2b2bce2d979dabf056781a524776db0d0fdc08
Eto010 <kirchenkovlad123@gmail.com>
2026-06-08 13:47:16 +0700

    programming t2 lab6 ex3

--- programming/lab6/main.c
+++ programming/lab6/main.c
@@ -4,28 +4,43 @@
 
 void * thread_func(void *arg)
 {
-    for(int i = 1; i < 6; i++)
+    char **thread_fill = (char**)arg;
+    for(int i = 0; i < 5; i++)
     {
-        printf("child thread: %d\n", i);
+        printf("%s\n", thread_fill[i]);
     }
     printf("the thread has ended\n");
 }
 
 int main()
 {
-    pthread_t thread;
-    if(pthread_create(&thread, NULL, thread_func, NULL))
+    pthread_t threads[4];
+    char *thread_fill[4][5] = {
+    {"message 11", "message 12", "message 13", "message 14", "message 15"},
+    {"message 21", "message 22", "message 23", "message 24", "message 25"},
+    {"message 31", "message 32", "message 33", "message 34", "message 35"},
+    {"message 41", "message 42", "message 43", "message 44", "message 45"}
+    };
+    for (int i = 0; i < 4; i++)
     {
-        printf("thread creation error\n");
-        exit(EXIT_FAILURE);
+        if(pthread_create(&threads[i], NULL, thread_func, &thread_fill[i]))
+        {
+            printf("thread creation error\n");
+            exit(EXIT_FAILURE);
+        }
     }
-    if (pthread_join(thread, NULL) != 0) {
+    for(int i = 0; i < 4; i++)
+    {
+        if (pthread_join(threads[i], NULL) != 0) 
+        {
             perror("thread waiting error\n");
             exit(EXIT_FAILURE);
         }
+    }
     for(int i = 1; i < 6; i++)
     {
         printf("parent thread: %d\n", i);
     }
     printf("the thread has ended\n");
 }
+
